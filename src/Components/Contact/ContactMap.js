a4be0b1a1048b53877b2dbf715f4b0a5fc1c5123
Francine <franmelopepe2@gmail.com>
2022-09-14 14:32:58 +0200

    contact page finisehd

---
+++ src/Components/Contact/ContactMap.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Map, Marker, ZoomControl } from "pigeon-maps";
+import styles from "./Contact.module.css";
+
+function ContactMap() {
+  const [center, setCenter] = useState([53.588416885931466, 9.972663369152546]);
+  const [zoom, setZoom] = useState(11);
+  return (
+    <>
+      <div className={styles.map_container}>
+        {/* <a
+          href="https://www.openstreetmap.org/#map=19/53.58842/9.97313"
+          target={"blank"}
+        > */}
+        <Map
+          height={"23.688rem"}
+          defaultCenter={[53.588416885931466, 9.972663369152546]}
+          defaultZoom={11}
+          center={center}
+          zoom={zoom}
+          onBoundsChanged={({ center, zoom }) => {
+            setCenter(center);
+            setZoom(zoom);
+          }}
+        >
+          <ZoomControl />
+          <Marker width={50} anchor={[53.588416885931466, 9.972663369152546]} />
+        </Map>
+        {/* </a> */}
+      </div>
+    </>
+  );
+}
+
+export default ContactMap;
