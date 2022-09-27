fe2c9fa11c6d28d981c7fff9b9615b65a98b97ee
Francine <franmelopepe2@gmail.com>
2022-09-27 12:51:55 +0200

    fruit catalogue created. Still working on it

--- src/Components/FruitCatalogue/FruitCatalogue.js
+++ src/Components/FruitCatalogue/FruitCatalogue.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import FruitSeason from "../Footer/FruitSeason";
+import styles from "./FruitCatalogue.module.css";
+import { Icon } from '@iconify/react';
+import NavbarPink from "../Navbar/NavbarPink";
+import Navbar from "../Navbar/Navbar";
+import React, { useState } from "react";
+import Home from '../Home/Home';
 
-function FruitCatalogue() {
+
+function FruitCatalogue(props) {
+
+  const BackgroundColor = () => {
+    const [backgroundColor, setbackgroundColor] = useState();
+  
+    if (<Home />) {
+      return <NavbarPink />;
+    } else {
+      return <Navbar />;
+    }
+  };
+
   return (
-    <div>FruitCatalogue</div>
-  )
+    <>
+      <div className={styles.fruit_catalogue_container}>
+        <div className={styles.fruit_catalogue_content}>
+          <FruitSeason  />
+          <Icon icon="bytesize:search" color="#707070" width="33" height="33" />
+          <Icon icon="clarity:shopping-cart-line" color="#707070" width="33" height="33" />
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default FruitCatalogue
+export default FruitCatalogue;
